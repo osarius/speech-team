790b07f73512f438cb578a13317c6010368ed56c
Weiyuan Lyu <109712965+lyuweiyu@users.noreply.github.com>
2023-04-18 21:51:08 -0400

    Update general.js

--- js/general.js
+++ js/general.js
@@ -9,3 +9,41 @@ document.querySelector("#drop").addEventListener("click",function(){
       console.log("yes")
     }
   });
+
+  window.addEventListener("load",function(){
+  var jsonLikesAndDislikes = window.localStorage.getItem("likesAndDislikes");
+  var likesAndDislikes = JSON.parse(jsonLikesAndDislikes);
+  if (likesAndDislikes) {
+    // Access likes and dislikes properties as needed
+    document.getElementById("likes").innerHTML = likesAndDislikes["likes"]
+    document.getElementById("dislikes").innerHTML = likesAndDislikes["dislikes"]
+  } else {
+    // No likes and dislikes information found
+    console.log("xixi")
+    var likesAndDislikes = {
+      "likes": 0,
+      "dislikes": 0
+    };
+
+    document.getElementById("likes").innerHTML = likesAndDislikes["likes"]
+    document.getElementById("dislikes").innerHTML = likesAndDislikes["dislikes"]
+    window.localStorage.setItem("likesAndDislikes", JSON.stringify(likesAndDislikes));
+  
+  }
+});
+
+function ClickLikes(){
+  var jsonLikesAndDislikes = window.localStorage.getItem("likesAndDislikes");
+  var likesAndDislikes = JSON.parse(jsonLikesAndDislikes);
+  likesAndDislikes["likes"] += 1;
+  document.getElementById("likes").innerHTML = likesAndDislikes["likes"]
+  window.localStorage.setItem("likesAndDislikes", JSON.stringify(likesAndDislikes));
+}
+
+function ClickDislikes(){
+  var jsonLikesAndDislikes = window.localStorage.getItem("likesAndDislikes");
+  var likesAndDislikes = JSON.parse(jsonLikesAndDislikes);
+  likesAndDislikes["dislikes"] += 1;
+  document.getElementById("dislikes").innerHTML = likesAndDislikes["dislikes"]
+  window.localStorage.setItem("likesAndDislikes", JSON.stringify(likesAndDislikes));
+}
